cf71ced743507f911b045558ca44c3efe1ac95e6
Dustin Kut Moy Cheung <dcheung@redhat.com>
2023-02-17 12:03:40 -0500

    Add jdbc integration

--- src/main/java/org/jboss/pnc/grogu/GreetingResource.java
+++ src/main/java/org/jboss/pnc/grogu/GreetingResource.java
@@ -1,5 +1,8 @@
-package org.jboss.pnc;
+package org.jboss.pnc.grogu;
 
+import org.jobrunr.scheduling.JobScheduler;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,9 +11,16 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+    @Inject
+    JobScheduler jobScheduler;
+
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+
+        jobScheduler.enqueue(() -> System.out.println("Hello"));
+
         return "Hello from RESTEasy Reactive";
     }
 }
